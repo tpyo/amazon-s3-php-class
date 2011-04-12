972c81c2eb9a47591f134f4e585f127e88bfee4b
Donovan Schonknecht <don@tpyo.net>
2011-04-12 17:46:00 +0200

    Updated copyright and comments

--- S3.php
+++ S3.php
@@ -2,7 +2,7 @@
 /**
 * $Id$
 *
-* Copyright (c) 2010, Donovan Schönknecht.  All rights reserved.
+* Copyright (c) 2011, Donovan Schönknecht.  All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are met:
@@ -1236,8 +1236,8 @@ class S3
 	/**
 	* Get a InvalidationBatch DOMDocument
 	*
-	* @internal Used to create XML in invalidate()
-	* @param array $paths Paths to objects to invalidate
+	* @internal Used to create XML in invalidateDistribution()
+	* @param array $paths Paths to objects to invalidateDistribution
 	* @return string
 	*/
 	private static function __getCloudFrontInvalidationBatchXML($paths, $callerReference = '0') {
