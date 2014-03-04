1fd1df0bffca03fa4df0e345079d3103426d6dd9
Donovan Schönknecht <don@tpyo.net>
2014-03-04 12:55:30 +0200

    Formatting/comment tweaks for #21

--- S3.php
+++ S3.php
@@ -2091,19 +2091,24 @@ final class S3Request
 	* - maxredirect is less or equal zero: no follow redirections
 	*
 	* @author zsalab
-	* @link http://de3.php.net/manual/de/function.curl-setopt.php#102121
+	* @link http://www.php.net/manual/de/function.curl-setopt.php#102121
 	* @param mixed $ch Resource
 	* @param integer &$maxredirect
 	* @return mixed
 	*/
-	private function curl_exec_follow(/*resource*/ $ch, /*int*/ &$maxredirect = null) {
+	private function curl_exec_follow(/*resource*/ $ch, /*int*/ &$maxredirect = null)
+    {
 		$mr = $maxredirect === null ? 5 : intval($maxredirect);
-		if (ini_get('open_basedir') == '' && ini_get('safe_mode') == 'Off') {
+		if (ini_get('open_basedir') == '' && ini_get('safe_mode') == 'Off')
+        {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $mr > 0);
 			curl_setopt($ch, CURLOPT_MAXREDIRS, $mr);
-		} else {
+		}
+        else
+        {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-			if ($mr > 0) {
+			if ($mr > 0)
+            {
 				$newurl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
 
 				$rch = curl_copy_handle($ch);
@@ -2111,28 +2116,33 @@ final class S3Request
 				curl_setopt($rch, CURLOPT_NOBODY, true);
 				curl_setopt($rch, CURLOPT_FORBID_REUSE, false);
 				curl_setopt($rch, CURLOPT_RETURNTRANSFER, true);
-				do {
+				do
+                {
 					curl_setopt($rch, CURLOPT_URL, $newurl);
 					$header = curl_exec($rch);
-					if (curl_errno($rch)) {
+					if (curl_errno($rch))
 						$code = 0;
-					} else {
+					else
+                    {
 						$code = curl_getinfo($rch, CURLINFO_HTTP_CODE);
-						if ($code == 301 || $code == 302) {
+						if ($code == 301 || $code == 302)
+                        {
 							preg_match('/Location:(.*?)\n/', $header, $matches);
 							$newurl = trim(array_pop($matches));
-						} else {
+						}
+                        else
+                        {
 							$code = 0;
 						}
 					}
 				} while ($code && --$mr);
 				curl_close($rch);
-				if (!$mr) {
-					if ($maxredirect === null) {
+				if (!$mr)
+                {
+					if ($maxredirect === null)
 						trigger_error('Too many redirects. When following redirects, libcurl hit the maximum amount.', E_USER_WARNING);
-					} else {
+                    else
 						$maxredirect = 0;
-					}
 					return false;
 				}
 				curl_setopt($ch, CURLOPT_URL, $newurl);
