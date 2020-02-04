3d69cf353b8b4d624cda0c4f6a918f580f862bfd
Simo Heinonen <simo@dealdash.com>
2020-02-04 15:18:38 +0200

    Array and string offset access syntax with curly braces is deprecated

--- S3.php
+++ S3.php
@@ -2511,7 +2511,7 @@ final class S3Request
 			elseif ($header == 'content-type')
 				$this->response->headers['type'] = $value;
 			elseif ($header == 'etag')
-				$this->response->headers['hash'] = $value{0} == '"' ? substr($value, 1, -1) : $value;
+				$this->response->headers['hash'] = $value[0] == '"' ? substr($value, 1, -1) : $value;
 			elseif (preg_match('/^x-amz-meta-.*$/', $header))
 				$this->response->headers[$header] = $value;
 		}
